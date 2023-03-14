fed95bd1807854ba509dbb2f12bf5e19eee59af8
Harshal Bhute <114854206+Harshal39@users.noreply.github.com>
2023-03-14 07:05:04 +0000

    Dynamic pages for each job

--- database.py
+++ database.py
@@ -20,5 +20,17 @@ def load_jobs_from_db():
     jobs.append(dict(zip(column_names, row)))
   return jobs
 
-  
+def load_job_from_db(id):
+  with engine.connect() as conn:
+    result = conn.execute(
+      text(f"SELECT * FROM jobs WHERE id = {id}"),
+      
+    )
+    rows = []
+    for row in result.all():
+      rows.append(row._mapping)
+      if len(rows) ==0:
+        return None
+      else:
+        return[dict(row) for row in rows]
   
