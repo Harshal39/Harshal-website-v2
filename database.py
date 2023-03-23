a92eeee892cb49d58a9126868ee9aefab4480ddf
Harshal Bhute <114854206+Harshal39@users.noreply.github.com>
2023-03-23 05:17:06 +0000

    Created application form and its submission

--- database.py
+++ database.py
@@ -23,14 +23,24 @@ def load_jobs_from_db():
 def load_job_from_db(id):
   with engine.connect() as conn:
     result = conn.execute(
-      text(f"SELECT * FROM jobs WHERE id = {id}"),
-      
+      text(f"SELECT * FROM jobs WHERE id = {id}")
     )
     rows = []
     for row in result.all():
       rows.append(row._mapping)
-      if len(rows) ==0:
+      if len(rows) == 0:
         return None
       else:
         return[dict(row) for row in rows]
-  
+
+
+def add_application_to_db(job_id, data):
+  with engine.connect() as conn:
+    query = text("INSERT INTO application (job_id, full_name,email,linkedin_url,education,work_experience,resume_url) VALUES (:job_id, :full_name, :email, :linkedin_url, :education, :work_experience, :resume_url)")
+
+    conn.execute(query, job_id=job_id, full_name=data['full_name'],
+            email=data['email'],
+            linkedin_url=data['linkedin_url'],
+            education=data['education'],
+            work_experience=data['work_experience'],
+            resume_url=data['resume_url'])
